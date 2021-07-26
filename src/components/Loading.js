61efa2964f59c64de026d5ff8fb638f6c679ff48
Ian Cossentino <icossentino@gmail.com>
2021-07-26 18:12:39 -0400

    add complement strand

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import helix from '../images/helix.svg'
 import getSequence from '../services/api/api_requests'
-import generateBase from '../services/sequence_format'
+import { generateBase, generateComplementaryBase } from '../services/sequence_format'
 
 const Loading = () => {
 
@@ -17,9 +17,18 @@ const Loading = () => {
       <img id="helix" className="animate w-40 h-40" src={helix} alt="dna" />
       <p>{sequence}</p>
       <span>{hello.toLocaleUpperCase()}</span>
-      {Array.prototype.map.call(sequence, (c) => {
-        return <img className="w-5 h-5" src={generateBase(c)} alt="dnaBase" />
-      })}
+      <div className="flex flex-col">
+        <div id="template-strand" className="flex flex-grow items-center">
+          {Array.prototype.map.call(sequence, (c) => {
+            return <img className="w-5 h-5 inline-flex" style={{ transform: "rotate(180deg)" }} src={generateBase(c)} alt="dnaBase" />
+          })}
+        </div>
+        <div id="complement-strand" className="flex flex-grow items-center">
+          {Array.prototype.map.call(sequence, (c) => {
+            return <img className="w-5 h-5 inline-flex" src={generateComplementaryBase(c)} alt="dnaBase" />
+          })}
+        </div>
+      </div>
     </div>
   )
 }
--- src/services/sequence_format.js
+++ src/services/sequence_format.js
@@ -1,16 +1,20 @@
-import adenine from '../images/bases/adenine.svg'
-import cytosine from '../images/bases/cytosine.svg'
-import guanine from '../images/bases/guanine.svg'
-import thymine from '../images/bases/thymine.svg'
+import adenine from '../images/bases/adenine_b.svg'
+import cytosine from '../images/bases/cytosine_b.svg'
+import guanine from '../images/bases/guanine_b.svg'
+import thymine from '../images/bases/thymine_b.svg'
 
 
 const baseMapper = { "A": adenine, "C": cytosine, "T": thymine, "G": guanine }
 
+const complementMapper = { "A": "T", "T": "A", "C": "G", "G": "C" }
 
-const generateBase = (baseChar) => {
-  const div = document.createElement('div')
+
+export const generateBase = (baseChar) => {
   const baseImage = baseMapper[baseChar]
   return baseImage
 }
 
-export default generateBase
+export const generateComplementaryBase = (baseChar) => {
+  const baseImage = baseMapper[complementMapper[baseChar]]
+  return baseImage
+}
