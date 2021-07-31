5c6695b115075e9b141d7f1a24dba69b240e7a05
Ian Cossentino <icossentino@gmail.com>
2021-07-31 00:35:02 -0400

    begin upload form state management

--- src/pages/upload.js
+++ src/pages/upload.js
@@ -7,8 +7,12 @@ const Upload = () => {
   return (
     <div>
       <Header />
-      <h1>Upload your sequence here</h1>
-      <UploadForm />
+      <div className="container mx-auto">
+        <h1 className="font-bold text-3xl to-blue-900">Upload your sequence here</h1>
+        <div className="flex items-center justify-center bg-blue-900">
+          <UploadForm />
+        </div>
+      </div>
     </div>
   )
 }
