f1395bd5765e8cc8342e6d6b98beb15e3d323299
Ian Cossentino <icossentino@gmail.com>
2021-08-21 13:32:53 -0400

    move sequences table to homepage

---
+++ src/components/tables/SequencesTable.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { useSequences } from '../../services/api/api_requests'
+
+export default function SequencesTable() {
+  const sequences = useSequences()
+  return (
+    <div className="flex justify-center align-middle">
+
+      <div className="flex flex-col">
+        <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div className="py-2 align-middle items-center sm:px-6 lg:px-8">
+            <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+              <table className="divide-y divide-gray-200">
+                <thead className="bg-gray-50">
+                  <tr>
+                    <th scope="col" className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
+                      Name
+                    </th>
+                    <th scope="col" className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
+                      Description
+                    </th>
+                    <th scope="col" className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
+                      FASTA Header
+                    </th>
+                  </tr>
+                </thead>
+                <tbody className="bg-white divide-y divide-gray-200">
+                  {sequences.map(s => (
+                    <tr key={s.id}>
+                      <td className="px-6 py-4 whitespace-nowrap">
+                        <Link className="link" to={`/sequences/${s.id}`}>
+                          <div className="text-sm text-gray-900">{s.name}</div>
+                        </Link>
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap">
+                        <div className="text-sm text-gray-900">{s.description}</div>
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap">
+                        <div className="text-sm text-gray-900">{s.fasta_header}</div>
+                      </td>
+                    </tr>))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,13 +5,19 @@ import helix from '../images/icons/helix.svg'
 import upload from '../images/icons/upload.svg'
 import useAuthListener from '../context/use-auth-listener'
 import Sidebar from '../components/Sidebar'
+import SequencesTable from '../components/tables/SequencesTable'
 
 const Home = () => {
   const user = useAuthListener()
   return user ? (
     <div>
       <Header />
-      <Sidebar />
+      <div className="flex">
+        <Sidebar />
+        <div className="w-5/6 flex justify-center">
+          <SequencesTable />
+        </div>
+      </div>
     </div>
   ) : <Redirect to="/login" />
 }
